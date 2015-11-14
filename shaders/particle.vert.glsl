499c84dba73dc69f72d55e370722c0c1a5861456
shrekshao <shrekshao@gmail.com>
2015-11-14 14:23:52 -0500

    delete gl_PointSize in vert shader

--- shaders/particle.vert.glsl
+++ shaders/particle.vert.glsl
@@ -8,7 +8,7 @@ uniform mat4 u_projMatrix;
 uniform mat4 u_modelView;
 
 void main(){
-   gl_PointSize = 10;                                                                                                                     
+                                                                                                                        
 
    //gl_Position = u_projMatrix * u_modelView * vec4(position, 1.0);
    gl_Position = vec4(position,1.0);
